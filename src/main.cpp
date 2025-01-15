ee71ab480d303202ba3e18e7ce4c575d107486c5
richardhosking <richardh@iinet.net.au>
2025-01-15 11:41:05 +1100

    RTC working with periodic interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include <Wire.h>
 #include "LowPower.h"
 #include "uSsensor.h"
+#include "SparkFun_RV8803.h"
 
 // Software Serial port pins for US sensor
 #define Tx 10
@@ -27,7 +28,7 @@ long BAUD = 9600;
 bool inverse = false;
 
 // RTC interrupt on pin 2 
-const byte RTC = 2;
+const byte RTCint = 2;
 
 // supply power to US transducer via a pin on the arduino
 // transducer takes about 15mA when powered up 
@@ -36,6 +37,19 @@ const byte sensor_power = 4;
 // Declare instance of US sensor
 USsensor sensor(Tx,Rx, inverse);
 
+// and RTC
+RV8803 rtc;
+//The below variables control what the date and time will be set to when set manually 
+int sec = 2;
+int minute = 40;
+int hour = 10; //Set things in 24 hour mode
+int date = 15;
+int month = 1;
+int year = 2025;
+int weekday = 3;
+
+long ticks = 3840;  // 1 minute between interrupts 15.625 mSec per tick
+
 // Instance of LowPowerClass
 // Seems to be declared implicitly
 
@@ -43,15 +57,15 @@ USsensor sensor(Tx,Rx, inverse);
 void serviceRTC ()
 {
   noInterrupts ();          // interrupts are disabled  
-  detachInterrupt (digitalPinToInterrupt (RTC)); // stop LOW interrupt on D2
-  digitalWrite (RTC, HIGH);  // 
+  detachInterrupt (digitalPinToInterrupt(RTCint)); // stop LOW interrupt on D2
+  digitalWrite (RTCint, HIGH);  // 
   interrupts ();           // interrupts allowed now, next instruction WILL be executed
 }  
 
 void sleepNow ()
 {
   noInterrupts ();          // interrupts are disabled
-  attachInterrupt (digitalPinToInterrupt (RTC), serviceRTC, LOW);  // wake up on low level on D2
+  attachInterrupt (digitalPinToInterrupt(RTCint), serviceRTC, LOW);  // wake up on low level on D2
   interrupts ();                                                   // interrupts allowed now, next instruction WILL be executed
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);             // sleep indefinitely, ADC and Brown out detector off
 }  
@@ -64,8 +78,8 @@ void setup(){
  Serial.println("Serial port active");
 
 // Set up pins - hardware interrupt on pin 2 for RTC
- pinMode(RTC, INPUT);
- digitalWrite (RTC, HIGH);
+ pinMode(RTCint, INPUT);
+ digitalWrite (RTCint, HIGH);
   
  pinMode (sensor_power, OUTPUT);
   
@@ -87,11 +101,56 @@ void setup(){
    Serial.println("Sensor not listening");
  }
  delay(20);
+
+ // Start I2C port for RTC
+ Wire.begin();
+  delay(20);
+
+  if (rtc.begin() == false)
+  {
+    Serial.println("RTC Device not found. Please check wiring. ");
+    while(1);
+  }
+  else
+  {
+    Serial.println("RTC Device found");
+  }
+   // Uncomment this section to manually set time on RTC
+   /*if (rtc.setTime(sec, minute, hour, weekday, date, month, year) == false) {
+    Serial.println("Something went wrong setting the time");
+    }
+  rtc.set24Hour(); */
+
+  // Set up RTC to wake up processor periodically
+  rtc.disableAllInterrupts();
+  rtc.clearAllInterruptFlags();//Clear all flags in case any interrupts have occurred.
+  rtc.setCountdownTimerFrequency(COUNTDOWN_TIMER_FREQUENCY_64_HZ);
+  rtc.setCountdownTimerClockTicks(ticks);  // 15.625 mSec per tick about 1 minute between interrupts
+  rtc.enableHardwareInterrupt(TIMER_INTERRUPT);
+  rtc.setCountdownTimerEnable(ALARM_ENABLE); //This will start the timer on the last clock tick of the I2C transaction
+    
+}
  
- }
   
 void loop()
 {  
+  
+  if(rtc.getInterruptFlag(FLAG_TIMER)){rtc.clearInterruptFlag(FLAG_TIMER);}
+  
+  if (rtc.updateTime() == true) //Updates the time variables from RTC
+  {
+    //String currentDate = rtc.stringDateUSA(); //Get the current date in mm/dd/yyyy format (we're weird)
+    String currentDate = rtc.stringDate(); //Get the current date in dd/mm/yyyy format
+    String currentTime = rtc.stringTime(); //Get the time
+    Serial.print(currentDate);
+    Serial.print(" ");
+    Serial.println(currentTime);
+  }
+  else
+  {
+    Serial.print("RTC read failed");
+  }
+  
   // Processor has been woken up by external interrupt 
   // Turn on US sensor
   digitalWrite(sensor_power, HIGH);
@@ -138,17 +197,16 @@ digitalWrite(sensor_power, LOW);
 
 // stop data from sensor or software serial interrupts will prevent sleep mode
 sensor.clearBuffer();
-// Need to call method from inherited library
 sensor.ser.end();
 
 // need if to allow for RTC line still being low 
-if (digitalRead(RTC) == HIGH){
+if (digitalRead(RTCint) == HIGH){
   sleepNow();
 }
 delay(100);
 Serial.println("Woken Up");
 
-delay(2100);
+//delay(2100);
 
 }
 
